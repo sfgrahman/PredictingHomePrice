0bb4cff0117fa3296234abdc76689766979eacf4
Moududur Shamim <sfgrahman35@gmail.com>
2022-10-31 21:40:10 +0600

    removed global variable

--- util.py
+++ util.py
@@ -7,44 +7,36 @@ __data_columns = None
 __model = None
 
 def get_estimated_price(location,sqft,bhk,bath):
+    p__data_columns = get_data_columns()
     try:
-        loc_index = __data_columns.index(location.lower())
+        loc_index = p__data_columns.index(location.lower())
     except:
         loc_index = -1
 
-    x = np.zeros(len(__data_columns))
+    x = np.zeros(len(p__data_columns))
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
     if loc_index>=0:
         x[loc_index] = 1
-
-    return round(__model.predict([x])[0],2)
+    model_predict = load_saved_artifacts()
+    return round(model_predict.predict([x])[0],2)
 
 
 def load_saved_artifacts():
-    print("loading saved artifacts...start")
-    global  __data_columns
-    global __locations
+    with open('banglore_home_prices_model.pickle', 'rb') as f:
+        __model = pickle.load(f)
+    return __model
 
+def get_location_names():
     with open("columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
-        #print(__locations)
-
-    global __model
-    if __model is None:
-        with open('banglore_home_prices_model.pickle', 'rb') as f:
-            __model = pickle.load(f)
-    print("loading saved artifacts...done")
-
-def get_location_names():
-    global __locations
-
     return __locations
 
 def get_data_columns():
-    global __data_columns
+    with open("columns.json", "r") as f:
+        __data_columns = json.load(f)['data_columns']
     return __data_columns
 
 if __name__ == '__main__':
